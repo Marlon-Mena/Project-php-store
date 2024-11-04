ae7801cdb559d705f9d47bb30a83186a22e16a22
Arthur Murilo Santos da Cunha <121124297+ArthurMurilo49@users.noreply.github.com>
2024-11-03 21:30:09 -0300

    Adicionei a parte dos produtos no Home_page

---
+++ js/home_page.js
@@ -0,0 +1,25 @@
+
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("open");
+}
+
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
+}
+
+// Inicializa o primeiro slide
+showSlide(currentIndex);
+
+// Muda de slide a cada 5 segundos (5000 ms)
+setInterval(nextSlide, 5000);
