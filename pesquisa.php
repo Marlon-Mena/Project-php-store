4c41472a075a43fbef4c04de9533a06a80ffcb6d
Marlon <marlon.mena.monteiro@gmail.com>
2024-11-05 14:42:34 -0300

    Adicionando os dados da tabela no html, porem está dando erro na linha 59 do pesquisa.php, mesmo assim está rodando

--- pesquisa.php
+++ pesquisa.php
@@ -15,13 +15,14 @@
             <nav class="navbar navbar-light bg-light">
                 <form class="form-inline" action="pesquisa.php" method="POST"> <!-- Corrigido "método" para "method" -->
                     <input class="form-control mr-sm-2" type="search" placeholder="Nome" aria-label="Search" name="busca">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Investigar</button>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
                 </form>
             </nav>
 
             <table class="table table-hover">
                 <thead>
                     <tr>
+                        <th scope="col">#</th>
                         <th scope="col">Nome</th>
                         <th scope="col">E-mail</th>
                         <th scope="col">password</th>
@@ -29,17 +30,41 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td scope="row">Nome</td>
+                        <th scope="row">id</th>
+                        <td>Nome</td>
                         <td>fulanodetal@prov.com.br</td>
                         <td>*******</td> 
                     </tr>
                 </tbody>
             </table>
 
-            <a href="index.php" class="btn btn-info">Voltar para o início</a>
+            <a href="index.html" class="btn btn-info">Voltar para o início</a>
         </div>
     </div>
 </div>
 </body>
 </html>
+
+<?php
+
+if(isset($_POST['busca'])) {
+    $pesquisa = $_POST['busca'];
+}
+else {
+    $pesquisa = '';
+}
+
+include "conexao.php";
+
+$sql = "SELECT * FROM pessoas WHERE nome LIKE '$Pesquisa%'";
+
+$dados = mysqli_query($conn, $sql);
+
+while ($linha = mysqli_fetch_assoc($dados)) {
+    foreach($linha as $key => $value) {
+        echo"$key: $value<br>";
+    }
+    echo"<hr>";
+}
+?>
 
